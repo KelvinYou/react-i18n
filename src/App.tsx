800b6130dd1bb3d1f29f8b98646cf15f98c641a4
KelvinYou <ykekelvin0220@gmail.com>
2023-02-27 08:02:48 +0800

    init with all function work but haven't arrange it

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,55 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import i18n from 'i18next';
+import { initReactI18next, useTranslation } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
 
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    // Specify the supported languages and their resources
+    resources: {
+      en: {
+        translation: {
+          greeting: 'Hello',
+        },
+      },
+      ch: {
+        translation: {
+          greeting: '你好',
+        },
+      },
+    },
+    // Define the fallback language and how it should be detected
+    fallbackLng: 'en',
+    detection: {
+      order: ['localStorage', 'navigator'],
+    },
+  });
+
+  // const changeLanguage = (lng: string) => {
+  //   i18n.changeLanguage(lng);
+  // };
+  
 function App() {
+  const { i18n } = useTranslation();
+  const { t } = useTranslation();
+  const handleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    i18n.changeLanguage(event.target.value);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        hi
+        <h1>{t('greeting')}</h1>
+        <select value={i18n.language} onChange={handleChange}>
+          <option value="en">English</option>
+          <option value="ch">中文</option>
+        </select>
+      </div>
     </div>
   );
 }
